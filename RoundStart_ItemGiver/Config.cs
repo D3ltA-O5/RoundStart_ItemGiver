653bf8e4149a9bd68504aae1a778725bb6a66ba2
D3ltA_O5 <cybercodeveloper@gmail.com>
2024-09-05 05:29:49 +0300

    First ver

---
+++ RoundStart_ItemGiver/Config.cs
@@ -0,0 +1,74 @@
+﻿using Exiled.API.Interfaces;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Exiled.API.Enums;
+using PlayerRoles;
+
+namespace ItemGiver
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+
+        public bool Debug { get; set; } = false;
+
+        public List<Group> Groups { get; set; } = new List<Group>
+        {
+            new Group
+            {
+                Name = "Group1",
+                Tag = "VIP",
+                Color = "red",
+                SteamIds = new List<string>
+                {
+                    "76561198012345678",  // Пример Steam ID64 игрока
+                    "76561198087654321"
+                },
+                Items = new List<string>
+                {
+                    "KeycardO5",  // O5-Level Keycard
+                    "Medkit"      // Medical Kit
+                }
+            },
+            new Group
+            {
+                Name = "Group2",
+                Tag = "Supporter",
+                Color = "blue",
+                SteamIds = new List<string>
+                {
+                    "76561198011223344"
+                },
+                Items = new List<string>
+                {
+                    "GunCOM15",   // COM-15 Pistol
+                    "Radio"       // Radio Device
+                }
+            }
+        };
+
+        public List<RoleTypeId> BlacklistedRoles { get; set; } = new List<RoleTypeId>
+        {
+            RoleTypeId.Scp049,
+            RoleTypeId.Scp079,
+            RoleTypeId.Scp096,
+            RoleTypeId.Scp106,
+            RoleTypeId.Scp173,
+            RoleTypeId.Scp939,
+            RoleTypeId.Scp3114
+        };
+    }
+
+    public class Group
+    {
+        public string Name { get; set; } = "NewGroup";
+
+        public string Tag { get; set; } = "Player";
+
+        public string Color { get; set; } = "white";
+
+        public List<string> SteamIds { get; set; } = new List<string>();  // Определение SteamIds
+
+        public List<string> Items { get; set; } = new List<string>();  // Определение Items
+    }
+}
---
+++ RoundStart_ItemGiver/Plugin.cs
@@ -0,0 +1,86 @@
+﻿using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+using System;
+using System.Linq;
+using UnityEngine;
+
+namespace ItemGiver
+{
+    public class RoundStart_ItemGiver : Plugin<Config>
+    {
+        public override string Name => "RoundStart_ItemGiver";
+        public override string Author => "D3ltA_O5";
+        public override Version Version => new Version(1, 0, 0);
+        public override Version RequiredExiledVersion => new Version(8, 11, 0);
+
+        public static RoundStart_ItemGiver Instance { get; private set; }
+
+        public override void OnEnabled()
+        {
+            Instance = this;
+            base.OnEnabled();
+
+            Log.Info("ItemGiverPlugin is being enabled...");
+            Exiled.Events.Handlers.Server.RoundStarted += OnRoundStarted;
+        }
+
+        public override void OnDisabled()
+        {
+            Log.Info("ItemGiverPlugin is being disabled...");
+            Exiled.Events.Handlers.Server.RoundStarted -= OnRoundStarted;
+            base.OnDisabled();
+        }
+
+        private void OnRoundStarted()
+        {
+            foreach (var group in Config.Groups)
+            {
+                foreach (var player in Player.List)
+                {
+                    // Извлечение чистого Steam ID без префиксов и суффиксов
+                    string steamId = player.UserId;
+
+                    if (steamId.Contains('@'))
+                    {
+                        steamId = steamId.Split('@')[0]; // Удаление суффикса @steam или другого
+                    }
+
+                    Log.Info($"Checking player {player.Nickname} with Steam ID {steamId} for group '{group.Name}'.");
+
+                    // Проверка на нахождение игрока в списке заблокированных ролей
+                    if (Config.BlacklistedRoles.Contains(player.Role.Type))
+                    {
+                        Log.Info($"Player {player.Nickname} with role {player.Role.Type} is blacklisted, skipping item giving.");
+                        continue;
+                    }
+
+                    if (group.SteamIds.Contains(steamId))
+                    {
+                        // Установка тега и цвета
+                        player.RankName = group.Tag;
+                        player.RankColor = group.Color;
+
+                        Log.Info($"Assigning tag '{group.Tag}' with color '{group.Color}' to player {player.Nickname}.");
+
+                        foreach (var itemName in group.Items)
+                        {
+                            if (Enum.TryParse(itemName, out ItemType itemType))
+                            {
+                                player.AddItem(itemType);
+                                Log.Info($"Given item '{itemType}' to player {player.Nickname}.");
+                            }
+                            else
+                            {
+                                Log.Warn($"Invalid item name '{itemName}' in config for group '{group.Name}'.");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Log.Info($"Player {player.Nickname} with Steam ID {steamId} is not in the group '{group.Name}', skipping.");
+                    }
+                }
+            }
+        }
+    }
+}
